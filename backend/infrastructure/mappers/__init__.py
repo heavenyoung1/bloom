41b53dfe52fa3a379c72fd0b23692d676a312d78
Evgeny <jacksonou3@gmail.com>
2025-10-30 15:50:28 +0300

    Реализованы методы для AttorneyRepository

---
+++ backend/infrastructure/mappers/__init__.py
@@ -0,0 +1,15 @@
+from .attorney_mapper import AttorneyMapper
+from .case_mapper import CaseMapper
+from .client_mapper import ClientMapper
+from .contact_mapper import ContactMapper
+from .document_mapper import DocumentMapper
+from .event_mapper import EventMapper
+
+__all__ = [
+    'AttorneyMapper',
+    'CaseMapper',
+    'ClientMapper',
+    'ContactMapper',
+    'DocumentMapper',
+    'EventMapper',
+]
--- backend/infrastructure/repositories/attorney_repo.py
+++ backend/infrastructure/repositories/attorney_repo.py
@@ -2,6 +2,7 @@ from sqlalchemy.orm import Session
 from sqlmodel import Session, select
 from typing import List, TYPE_CHECKING
 from backend.domain.entities.attorney import Attorney
+from backend.infrastructure.mappers import AttorneyMapper
 from core.exceptions import EntityNotFoundException, DatabaseErrorException
 
 from ..repositories.interfaces import IAttorneyRepository
@@ -14,51 +15,88 @@ class AttorneyRepository(IAttorneyRepository):
     def __init__(self, session: Session):
         self.session = session
 
+    def save(self, attorney: Attorney) -> bool:
+        '''Сохранить юриста в базе данных, если он не существует.'''
+        try:
+            statement = select(Attorney).where(attorney.id == id)
+            attorney_searched = self.session.exec(statement).first()
+
+            if attorney_searched is None:  # Если юрист не найден, добавляем нового
+                orm_attorney = AttorneyMapper.to_orm(domain=attorney)
+                self.session.add(orm_attorney)
+                return True
+            else:
+                return False  # Юрист с таким ID уже существует
+        except Exception as e:
+            raise DatabaseErrorException(f'Ошибка при сохранении юриста: {str(e)}')
+
     # Целесообразность наличия данного метода пока не подтверждена
     def get(self, id: int) -> 'Attorney':
         '''Получить адвоката по ID.'''
         try:
             statement = select(Attorney).where(Attorney.id == id)
             attorney = self.session.exec(statement).first()
+
             if not attorney:
-                raise EntityNotFoundException(
-                    'Юрист'
-                )  # Выбрасываем исключение, если не найдено
-            else:
-                return Attorney
+                raise EntityNotFoundException('Юрист не найден')
+
+            return attorney
         except Exception as e:
-            raise DatabaseErrorException(
-                f'Ошибка при получении юристов: {str(e)}'
-            )  # Логируем или обрабатываем ошибки базы данных
+            raise DatabaseErrorException(f'Ошибка при получении юриста: {str(e)}')
 
     # Целесообразность наличия данного метода пока не подтверждена
     def get_all(self) -> List['Attorney']:
+        '''Получить всех юристовю.'''
         try:
             statement = select(Attorney)
             attorneys = self.session.exec(statement).all()
+
             if not attorneys:
-                raise EntityNotFoundException(
-                    'Юрист'
-                )  # Выбрасываем исключение, если не найдено
+                raise EntityNotFoundException('Юристы не найдены')
+
+            return attorneys
         except Exception as e:
             raise DatabaseErrorException(
                 f'Ошибка при получении списка юристов: {str(e)}'
-            )  # Логируем или обрабатываем ошибки базы данных
+            )
 
-    def update(self) -> 'Attorney':
+    def update(self, id: int, updated_attorney: Attorney) -> 'Attorney':
         try:
             statement = select(Attorney).where(Attorney.id == id)
             attorney = self.session.exec(statement).first()
+
             if not attorney:
-                raise EntityNotFoundException(
-                    'Юрист'
-                )  # Выбрасываем исключение, если не найдено
-            else:
-                pass
+                raise EntityNotFoundException('Юрист не найден')
+
+            # Обновляем поля адвоката
+            attorney.first_name = updated_attorney.first_name
+            attorney.last_name = updated_attorney.last_name
+            attorney.patronymic = updated_attorney.patronymic
+            attorney.email = updated_attorney.email
+            attorney.phone = updated_attorney.phone
+            attorney.password_hash = updated_attorney.password_hash
+            attorney.is_active = updated_attorney.is_active
+            attorney.updated_at = updated_attorney.updated_at
+
+            # Коммитим изменения
+            self.session.add()
+            return attorney  # Возвращаем обновленного адвоката
         except Exception as e:
             raise DatabaseErrorException(
-                f'Ошибка при получении юристов: {str(e)}'
-            )  # Логируем или обрабатываем ошибки базы данных
+                f'Ошибка при обновлении данных юриста: {str(e)}'
+            )
 
-    def delete(self) -> 'Attorney':
-        pass
+    def delete(self, id: int) -> bool:
+        '''Удалить юриста по ID'''
+        try:
+            statement = select(Attorney).where(Attorney.id == id)
+            attorney = self.session.exec(statement).first()
+
+            if not attorney:
+                raise EntityNotFoundException('Юрист не найден')
+                # Удаляем адвоката
+
+            self.session.delete(attorney)
+            return True  # Возвращаем True, если удаление прошло успешно
+        except Exception as e:
+            raise DatabaseErrorException(f'Ошибка при удалении юриста: {str(e)}')
