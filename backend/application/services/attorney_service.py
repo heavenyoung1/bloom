f8ceb1aae675c840b36f81cc084b9548977dbaab
Evgeny <jacksonou3@gmail.com>
2025-11-13 13:58:06 +0300

    Добавлениее нового метода для AttorneyService и добавление новых файлов для Фабрик сущностей

--- backend/application/services/attorney_service.py
+++ backend/application/services/attorney_service.py
@@ -6,6 +6,8 @@ from backend.application.dto.attorney import (
     AttorneyListItemDTO,
 )
 from backend.domain.entities.attorney import Attorney
+from backend.application.validators.attorney_validator import AttorneyValidator
+from backend.domain.factories.attorney_factory import AttorneyFactory
 from backend.core.exceptions import EntityNotFoundException, ValidationException
 from backend.core.logger import logger
 import hashlib
@@ -16,7 +18,35 @@ class AttorneyService:
 
     def __init__(self, uow: AsyncUnitOfWork):
         self.uow = uow
-        self.uow
+        self.validator = AttorneyValidator(uow.attorney_repo)
+        self.factory = AttorneyFactory()
 
     async def create_attorney(self, data: CreateAttorneyDTO) -> 'AttorneyResponseDTO':
-        '''Создать юриста'''
+        '''
+        Создать нового юриста.
+
+        Шаги:
+        1. Валидируем данные (Validator)
+        2. Создаём Entity (Factory)
+        3. Сохраняем через Repository (UoW)
+        4. Возвращаем DTO
+        '''
+
+        # 1. Валидация
+        await self.validator.validate_on_create(data)
+
+        # 2. Создание Entity через Factory
+        attorney = self.factory.create(
+            license_id=data.attorney_id,
+            first_name=data.first_name,
+            last_name=data.last_name,
+            patronymic=data.patronymic,
+            email=data.email,
+            phone=data.phone,
+            password_hash=data.password,  #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        )
+
+        # 3. Сохраняем в БД
+        saved_attorney = await self.uow.attorney_repo.save(attorney)
+
+        return AttorneyResponseDTO.model_validate
